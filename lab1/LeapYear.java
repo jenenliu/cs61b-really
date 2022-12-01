4af1c6cc205d4c010fe9fd006681b2997c9bf6ca
jenenliu <jenenliu@gmail.com>
2022-12-01 17:15:27 +0800

    add isLeapYear function

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,15 @@ public class LeapYear {
         }
     }
 
+    /** isLeapYear check if year is leap year
+     *
+     * @param year
+     * @return if year is leap year
+     */
+    private static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
